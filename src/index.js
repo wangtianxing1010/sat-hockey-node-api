3b25c20fb31404a80c4f0e6e4ba9c0b224bef4c9
wangtianxing1010 <16573740+wangtianxing1010@users.noreply.github.com>
2019-07-27 21:34:24 -0400

    eslint setup

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ const app = express();
 
 app.post('/api/auth', (req, res) => {
     res.status(400).json({ errors: { global: "Invalid Credentials" } });
-})
+});
+
+app.get('/api/auth', (req, res) => {
+    res.status(400).json({ errors: { global: "Invalid Credentials" } });
+});
 
 app.get("/*", (req, res)=>{
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(8080, ()=> console.log("Running on localhost 8080"))
+app.listen(8080, () => console.log("Running on localhost 8080"))
