4b25d7798900c3f2bd6e34a1bcd2819d6665f74a
wangtianxing1010 <16573740+wangtianxing1010@users.noreply.github.com>
2019-07-27 23:00:45 -0400

    user authentication

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import express from "express";
 import path from "path";
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import auth from './routes/auth';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
+app.use(bodyParser.json()); 
+mongoose.connect(process.env.MONGODB_URL, { useCreateIndex: true , useNewUrlParser: true });
 
-app.post('/api/auth', (req, res) => {
-    res.status(400).json({ errors: { global: "Invalid Credentials" } });
-});
+app.use('/api/auth', auth); 
 
-app.get('/api/auth', (req, res) => {
-    res.status(400).json({ errors: { global: "Invalid Credentials" } });
-});
+// app.post('/api/auth', (req, res) => {
+//     res.status(400).json({ errors: { global: "Invalid Credentials" } });
+// });
 
 app.get("/*", (req, res)=>{
     res.sendFile(path.join(__dirname, 'index.html'));
